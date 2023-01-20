e9a86177cdefc7da9b5468ac212d27a35bed4f64
Eshita <edas25564@gmail.com>
2023-01-20 11:32:31 +0530

    "Till 20th Jan 11:32am"

--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -3,14 +3,22 @@ import React, { Component } from 'react'
 export class Newsitem extends Component {
  
   render() {
-    let{title, description, imageUrl, newsUrl} = this.props
+    let{title, description, imageUrl, newsUrl,author,date,source} = this.props
     return (
       <div>
-            <div className="card" style={{width: "18rem"}}>
+            <div className="card">
+            <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'80%',zIndex:'1'}}>
+                   {source}
+                </span>
               <img src={imageUrl} className="card-img-top" alt=""/>
               <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{description}</p>
+                <p className="card-text"><small class="text-primary">By {!author?"Unknown":author}</small></p>
+                <p className="card-text"><small class="text-danger">on {new Date(date).toGMTString()}</small></p>
+
+
+                
                 <a rel="noreferrer" href={newsUrl}  target="_blank" className="btn btn-sm btn-dark">Read More</a>
               </div>
             </div>
